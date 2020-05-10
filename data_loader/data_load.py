22a570aa3f81b66ef92be0c2511724845dfd1c96
xuzhang <xuzhang2010@gmail.com>
2020-05-10 01:25:00 -0400

    rename

--- data_loader/data_load.py
+++ data_loader/data_load.py
@@ -4,7 +4,7 @@ import pandas as pd
 class DataLoader:
     def __init__(self, config):
         self.config = config
-        BASE_PATH = self.config['base_path']
+        BASE_PATH = self.config['data_path']
         # load data here
         self.fnc_df = pd.read_csv(f"{BASE_PATH}/fnc.csv")
         self.loading_df = pd.read_csv(f"{BASE_PATH}/loading.csv")
